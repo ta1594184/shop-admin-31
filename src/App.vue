20c2813a5f374465897d0acc07890b6d449b19a1
wgq <211669797@qq.com>
2019-06-08 15:23:59 +0800

    第一次提交

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    index
+     <el-button type="primary">主要按钮</el-button>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios"
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+ mounted(){
+  //  $axios({
+  //    url:"https://api.github.com/users",
+  //    method:"get",
+  //  }).then(res=>{
+  //    console.log(res)
+  //  })
+
+  //绑定axios原型方法
+  this.$axios({
+     url:"https://api.github.com/users",
+     method:"get",
+   }).then(res=>{
+     console.log(res)
+   })
+ }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import Element from "element-ui"
+
+import axios from "axios"
+
+import 'element-ui/lib/theme-chalk/index.css';
+
 Vue.config.productionTip = false
+
+Vue.prototype.$axios=axios
+
+Vue.use(Element)
 
 new Vue({
   render: h => h(App),
